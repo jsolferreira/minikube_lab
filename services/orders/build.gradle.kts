c3a050da8ab7d291baae0a08aad76eaf86017199
jsolferreira <jsolferreira@gmail.com>
2026-02-28 22:43:19 +0000

    Add flyway migrations

--- services/orders/build.gradle.kts
+++ services/orders/build.gradle.kts
@@ -18,8 +18,15 @@ repositories {
 }
 
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-flyway")
     implementation("org.springframework.boot:spring-boot-starter-webmvc")
+    implementation("org.flywaydb:flyway-database-postgresql")
+
+    runtimeOnly("org.postgresql:postgresql")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-flyway-test")
     testImplementation("org.springframework.boot:spring-boot-starter-webmvc-test")
+
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
